4435f8894daf1877ba64b09aaa69e3b155ba2caa
Jalal Hussain <jalal143880@gmail.com>
2024-12-21 19:47:05 +0500

    Added new courses data with detailed information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
+
+import CoursesPage from "./pages/courses/page";
+
 export default function Home() {
   return (
     <>
-    <h1>Wellcome to Home page</h1>
+      <CoursesPage />
     </>
   );
 }
