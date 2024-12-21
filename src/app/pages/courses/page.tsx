869ff3a5eecde0b9816951551c8faadd405fa4da
Jalal Hussain <jalal143880@gmail.com>
2024-12-21 19:46:47 +0500

    Added CoursesPage component and updated courses data

--- src/app/pages/courses/page.tsx
+++ src/app/pages/courses/page.tsx
@@ -1,14 +1,21 @@
-import Link from "next/link";
+import Card from "@/components/card";
+import { coursesData } from "@/data/coursesData";
+import { Course } from "@/types/courseTypes";
 
 function CoursesPage() {
   return (
-    <div className="center courses-card">
-      <div className="  courses-card__content">
-        <h3 className="courses-card__title">Course Title</h3>
-        <p className="courses-card__description">Course Description</p>
-        <Link href="/courses">Details</Link>
-      </div>
-    </div>
+    <main className="bg-white min-h-screen py-8">
+      <header className="flex items-center justify-center mb-8">
+        <div className="w-1/2 md:w1/3 py-4 px-2 rounded-lg text-center bg-[#2A327D] text-white">
+          <h1 className="text-xl md:text-2xl font-bold">OUR COURSES</h1>
+        </div>
+      </header>
+      <section className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4">
+        {coursesData.map((course: Course) => (
+          <Card key={course.id} course={course} />
+        ))}
+      </section>
+    </main>
   );
 }
 
